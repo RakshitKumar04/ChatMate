dbc68e4066b7093fed6cc0e33aea4b37e953ae6d
Rakshit Sharma <kumarrakshit0402@gmail.com>
2023-11-22 00:12:22 +0530

    Gapi

---
+++ go-server/gapi/server.go
@@ -0,0 +1,34 @@
+package gapi
+
+import (
+	"fmt"
+
+	"github.com/RakshitKumar04/ChatMate/tree/master/go-server/db"
+	"github.com/RakshitKumar04/ChatMate/tree/master/go-server/pb"
+	"github.com/RakshitKumar04/ChatMate/tree/master/go-server/token"
+	"github.com/RakshitKumar04/ChatMate/tree/master/go-server/utils"
+	"google.golang.org/grpc/status"
+)
+
+type Server struct {
+	pb.GrpcServerServiceServer
+	config       utils.ViperConfig
+	dbCollection db.MongoCollections
+
+	tokenMaker token.Maker
+}
+
+func NewServer(config utils.ViperConfig, dbCollection db.MongoCollections) (*Server, error) {
+	tokenMaker, err := token.NewJwtMaker(config.TokkenStructureKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:       config,
+		dbCollection: dbCollection,
+		tokenMaker:   tokenMaker,
+	}
+
+	return server, nil
+}
