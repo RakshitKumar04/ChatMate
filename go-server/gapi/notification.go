dd9f4fb26d2e51b6b203b14deed414faeacc9b14
Rakshit Sharma <kumarrakshit0402@gmail.com>
2023-11-25 13:30:38 +0530

    SErvices

---
+++ go-server/gapi/notification.go
@@ -0,0 +1,44 @@
+package gapi
+
+import (
+	"fmt"
+
+	"github.com/RakshitKumar04/ChatMate/tree/master/go-server/pb"
+	"github.com/djsmk123/server/services"
+)
+
+// GetNotifications is a gRPC service method that streams notifications to the client.
+func (server *Server) GetNotifications(req *pb.EmptyRequest, stream pb.GrpcServerService_GetNotificationsServer) error {
+	fmt.Println("Notification service started")
+
+	// Create a channel for receiving notifications
+	notificationCh := make(chan *pb.NotificationMessage)
+
+	// Start the background service to listen for new user additions
+	go services.NotificationNewUser(server.dbCollection.Users, notificationCh)
+
+	fmt.Println("Notification service created")
+
+	// Continuously listen for events
+	for {
+		select {
+		case <-stream.Context().Done():
+			// Client disconnected, close the channel and exit
+			close(notificationCh)
+			return nil
+		case notification, ok := <-notificationCh:
+			if !ok {
+				// The channel has been closed, exit the loop
+				return nil
+			}
+			// Send the received notification to the client
+			if err := stream.Send(&pb.NotificationMessage{
+				Title:       notification.Title,
+				Id:          int32(notification.Id),
+				Description: notification.Description,
+			}); err != nil {
+				return err
+			}
+		}
+	}
+}
